e73684ee5aa49823c43b7b042ae75e6c48703509
satcse19 <81626145+satcse19@users.noreply.github.com>
2022-02-11 22:39:44 +0600

    Jqury basic hide and show

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".bt1").click(function () {
+    $(".ers").hide();
+  });
+});
+
+$(document).ready(function () {
+  $(".bt2").click(function () {
+    $(".ers").show();
+  });
+});
